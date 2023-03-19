1f8c5a5a1b2418def47c81fe853ca73886259061
Halil Özel <halilozel1903@gmail.com>
2023-03-19 22:01:22 +0300

    Leap Year code added.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,17 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun isLeapYear(year: Int?): Boolean {
+    return if (year != null) {
+        (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)
+    } else {
+        false
+    }
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    print("Enter favorite year: ")
+    val year = readlnOrNull()?.toInt()
+    if (isLeapYear(year)) {
+        println("$year is a leap year.")
+    } else {
+        println("$year is not a leap year.")
+    }
 }
