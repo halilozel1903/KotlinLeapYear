672cf7defc33a2a5e7ebb07ba04c5351dbeab755
Halil Ozel <halilozel1903@gmail.com>
2023-11-05 17:34:36 +0300

    Short code made for Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,7 @@
-fun isLeapYear(year: Int?): Boolean {
-    return if (year != null) {
-        (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)
-    } else {
-        false
-    }
-}
+fun isLeapYear(year: Int?) = (year != null) && (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)
 
 fun main() {
     print("Enter favorite year: ")
     val year = readlnOrNull()?.toInt()
-    if (isLeapYear(year)) {
-        println("$year is a leap year.")
-    } else {
-        println("$year is not a leap year.")
-    }
+    println("${year ?: "Invalid input"} is ${if (isLeapYear(year)) "a leap" else "not a leap"} year.")
 }
